3d8e4079d718873fe97d0f34c32f6b2c72b05d98
NicholasH15 <n_henricksen@u.pacific.edu>
2025-02-07 19:16:41 -0800

    something

--- scanner.py
+++ scanner.py
@@ -13,14 +13,34 @@ def parseLine(line):
     print(line)
     #make each element a tuple with token and type
     tokenList = []
+    token = ""
     indexStart = 0
     indexEnd = 0
+    count = 0
     for char in line:
         #if char == upper or lower, keep going until non alphanumeric
         #if char == number and its the start of token, continue until non number
         #if char == symbol, it's its own token and next should be something else
         #if char == whitespace, end last token and start new
         #if char == non recognizable symbol, error
+        text = line[0:count + 1]
+        resultID = bool(re.fullmatch(identifier, text))
+        resultNum = bool(re.fullmatch(number, text))
+        resultSymbol = bool(re.fullmatch(symbol, text))
+
+        if resultID:
+            token = text
+        elif resultNum:
+            token = text
+        elif resultSymbol:
+            token = text
+        
+        #else error
+
+        count += 1
+
+        
+        
         currIndex = char.index()
         if char.isalpha:
             continue
