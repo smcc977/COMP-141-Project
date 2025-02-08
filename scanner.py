4b8cb1d4996f1014d461607bc0c627ce903b8696
NicholasH15 <n_henricksen@u.pacific.edu>
2025-02-07 18:22:56 -0800

    add function

--- scanner.py
+++ scanner.py
@@ -3,3 +3,12 @@ import sys
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 
+def parseLine(line):
+    None
+
+if __name__ == "__main__":
+    inputList = []
+    for line in input_file:
+        inputList.append(line)
+    for list in inputList:
+        parseLine(line)
