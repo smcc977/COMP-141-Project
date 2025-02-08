d531f94e833b36c4b0614f44b7eeabb6b530da08
NicholasH15 <n_henricksen@u.pacific.edu>
2025-02-07 19:06:46 -0800

    pseudo

--- scanner.py
+++ scanner.py
@@ -1,14 +1,42 @@
 import sys
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 input_file = sys.argv[1]
 output_file = sys.argv[2]
 
+
+
 def parseLine(line):
     print(line)
-    index = 0
+    #make each element a tuple with token and type
+    tokenList = []
+    indexStart = 0
+    indexEnd = 0
     for char in line:
-        if index == 0:
-            None
+        #if char == upper or lower, keep going until non alphanumeric
+        #if char == number and its the start of token, continue until non number
+        #if char == symbol, it's its own token and next should be something else
+        #if char == whitespace, end last token and start new
+        #if char == non recognizable symbol, error
+        currIndex = char.index()
+        if char.isalpha:
+            continue
+        if char == " ":
+            tokenList.append(line[0:char + 1])
+            tokenList    
+        
 
 if __name__ == "__main__":
     inputList = []
