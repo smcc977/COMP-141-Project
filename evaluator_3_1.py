f12cd76a5866d183c0412392d3f6980d3ad6975a
smcc977 <sammcc977@gmail.com>
2025-04-05 12:49:55 -0700

    updates for new assignment

---
+++ evaluator_3_1.py
@@ -0,0 +1,9 @@
+"""
+COMP 141: Course Project
+Phase 3.1: Evaluator for Lexp
+
+This program integrates a scanner (Phase 1.2) and a parser for Lexp.
+It reads an input file (with a single expression), tokenizes it, and then parses
+the tokens to build an abstract syntax tree (AST). The tokens and AST are printed
+to the output file.
+"""
